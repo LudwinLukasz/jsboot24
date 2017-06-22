92255d4b3d5279c0d19022b8bed66644d50486e3
Lukasz Ludwin <gorli@interia.pl>
2017-06-22 22:34:32 +0200

    third commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var Counter = React.createClass({
 	getDefaultProps: function() {
 		console.log(this.props);
-		console.log("getDefaultProps: definicja propsow identycznych dla wszystkich instancji klasy")
+		console.log('getDefaultProps: definicja propsow identycznych dla wszystkich instancji klasy')
 		return {
 			test: 'nowy props'
 		};
@@ -22,9 +22,9 @@ var Counter = React.createClass({
 		});
 	},
 	componentWillMount: function() {
-		console.log("componentWillMount: nie zmienia tego co ma być renderowane?")
+		console.log('componentWillMount: nie zmienia tego co ma być renderowane?');
 		this.setState({
-		counter: 3
+			counter: 3
 		});
 	},
 	render: function() {
@@ -36,31 +36,31 @@ var Counter = React.createClass({
 		);
 	},
 	componentDidMount: function(){
-		console.log("componentDidMount: pobieram dane, moge uzyc stworzony w render element")
+		console.log('componentDidMount: pobieram dane, moge uzyc stworzony w render element');
 	},
 	componentWillReceiveProps: function() {
-		console.log("componentWillReceiveProps: wywolana jak komponent otrzyma nowe propsy")
+		console.log('componentWillReceiveProps: wywolana jak komponent otrzyma nowe propsy');
 	},
 	shouldComponentUpdate: function() {
-		console.log("shouldComponentUpdate: wywolana jak warto updatowac - tu: lubie 5-tki :) ")
-			if (this.state.counter === 5) {
-				return false
-			}
-		return true
+		console.log('shouldComponentUpdate: wywolana jak warto updatowac - tu: lubie 5-tki :)');
+		if (this.state.counter === 5) {
+			return false;
+		}
+		return true;
 	},
 	componentWillUpdate: function(){
-		console.log("componentWillUpdate")
+		console.log('componentWillUpdate');
 	},
 	componentDidUpdate: function(){
-		console.log("componentDidUpdate")
+		console.log('componentDidUpdate');
 	},
 	 componentWillUnmount: function() {
-		console.log("componentWillUnmount")
+		console.log('componentWillUnmount');
 	 }
 });
 var buttons = React.createElement('div', {}, 
 	React.createElement(Counter,{}),
 	React.createElement(Counter,{})
-	)
+	);
 ReactDOM.render(buttons, document.getElementById('app'));
 setTimeout(function(){ReactDOM.unmountComponentAtNode(document.getElementById('app')); }, 30000);
